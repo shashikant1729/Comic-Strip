4ee94889ba30bb8437172cabb7c1aa9db3b41704
Shashi Kant <shashi1729kant@gmail.com>
2023-11-23 20:53:01 +0530

    initiate

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import shoppy from "../assets/images/download-removebg.png";
+import { NavLink } from "react-router-dom";
+const Header = (props) => {
+	// console.log('header : ', props.data[0]);
+	return (
+		<>
+			<div className="flex flex-row p-4 right-0 relative text-lg w-full h-16 text-white  drop-shadow-xl backdrop-blur-sm text-center justify-between">
+				<div className="flex gap-3 text-xl text-white">
+					<img src={shoppy} alt="logo" className="w-8 h-8" />
+					<p className="font-semibold">Shoppy</p>
+				</div>
+				<div className="flex flex-row gap-6">
+					<div>
+						{/* <div className="relative">
+							<div className="text-white font-thin px-4	 rounded-lg shadow-2xl">
+								Transition
+							</div>
+							<div className="absolute top-0 right-0 -mr-1 -mt-1 w-4 h-4 rounded-full bg-blue-700 animate-ping"></div>
+							<div className="absolute top-0 right-0 -mr-1 -mt-1 w-4 h-4 rounded-full bg-blue-700"></div>
+						</div> */}
+					</div>
+					<div className="flex flex-row gap-2">
+						<div>
+							<NavLink to="/about">About</NavLink>
+						</div>
+						<NavLink to="/">
+							<div className="relative">
+								<div className="text-white font-thin px-4	 rounded-lg shadow-2xl">
+									<img src={shoppy} alt="logo" className="w-8 h-8" />
+								</div>
+								<div
+									className={`absolute top-0 right-0 -mr-1 -mt-2 w-6 h-6  rounded-full bg-red-500  ${
+										props.data.length !== 0 ? `animate-ping` : ``
+									}`}
+								></div>
+								<div className="absolute top-0 right-0 -mr-1 -mt-2 w-6 h-6 rounded-full text-center text-sm bg-red-500">
+									{props.data.length}
+								</div>
+							</div>
+						</NavLink>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Redux
+import { Provider } from "react-redux";
+import store from "./Redux/Store/store";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
